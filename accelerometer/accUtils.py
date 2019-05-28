1e2ebb84e50d98db095c7d40adfe3b00bd8c0d90
Shing Chan at BDI <shingc@ndph.ox.ac.uk>
2019-05-28 16:41:15 +0100

    use None instead of ""

--- accelerometer/accUtils.py
+++ accelerometer/accUtils.py
@@ -33,14 +33,14 @@ def formatNum(num, decimalPlaces):
 
     fmt = '%.' + str(decimalPlaces) + 'f'
     return float(fmt % num)
-    
+
 
 
 def toScreen(msg):
-    """Print msg str prepended with current time 
+    """Print msg str prepended with current time
 
     :param str mgs: Message to be printed to screen
-    :return: Print msg str prepended with current time 
+    :return: Print msg str prepended with current time
     :rtype: void
 
     :Example:
@@ -54,21 +54,21 @@ def toScreen(msg):
 
 
 
-def generateTimeSeries(epochPD, tsFile, timeSeriesDateColumn = False, 
+def generateTimeSeries(epochPD, tsFile, timeSeriesDateColumn = False,
     epochPeriod = 30, activityClassification = None, labels = []):
     """Write .csv.gz time series file of <e> activity epoch data to <tsFile>
 
-    These time-series files describe a participant's accelerometer measured 
-    physical activity intensity every <epochPeriod> seconds. Each measurement 
-    represents the average vector magnitude across all samples recorded in each 
+    These time-series files describe a participant's accelerometer measured
+    physical activity intensity every <epochPeriod> seconds. Each measurement
+    represents the average vector magnitude across all samples recorded in each
     <epochPeriod>. This file may be useful for those interested in subsequently
     analysing accelerometer measured physical activity by time.
 
-    The first column contains the acceleration value where the units of 
-    measurement are milli-gravity. If <activityClassification> is requested, the 
+    The first column contains the acceleration value where the units of
+    measurement are milli-gravity. If <activityClassification> is requested, the
     next column indicates the most likely activity state for any given
-    <epochPeriod>. The final column indicates whether this is an actual 
-    measurement (value = <blank>) or imputed (value = 1). 
+    <epochPeriod>. The final column indicates whether this is an actual
+    measurement (value = <blank>) or imputed (value = 1).
 
     Each files is typically 760KB in the raw state and compresses to around 170KB.
 
@@ -76,11 +76,11 @@ def generateTimeSeries(epochPD, tsFile, timeSeriesDateColumn = False,
     :param str tsFile: Output filename for .csv.gz file
     :param bool timeSeriesDateColumn: Write time associated with each reading
     :param datetime startTime: Remove data before this time in analysis
-    :param datetime endTime: Remove data after this time in analysis 
+    :param datetime endTime: Remove data after this time in analysis
     :param int epochPeriod: size of epoch time window (in seconds)
     :param bool activityClassification: Write out most likely activity state
     :param list(str) labels: Activity state labels
-    
+
     :return: Write .csv.gz time series file of activity epoch data to <tsFile>
     :rtype: void
 
@@ -91,7 +91,7 @@ def generateTimeSeries(epochPD, tsFile, timeSeriesDateColumn = False,
     >>> accUtils.generateTimeSeries(e, "timeSeriesFile.csv.gz")
     <file written to timeSeriesFile.csv.gz>
     """
-    
+
     startTime = pd.to_datetime(epochPD.index.values[0])
     endTime = pd.to_datetime(epochPD.index.values[-1])
     # resample index so we can add imputed data
@@ -104,7 +104,7 @@ def generateTimeSeries(epochPD, tsFile, timeSeriesDateColumn = False,
     tsHead += e.index.min().strftime('%Y-%m-%d %H:%M:%S') + ' - '
     tsHead += e.index.max().strftime('%Y-%m-%d %H:%M:%S') + ' - '
     tsHead += 'sampleRate = ' + str(epochPeriod) + ' seconds'
-    
+
     # write time series file
     if len(e['accImputed']) > 0:
         # fill with vm, imputed data used where needed, convert to mg units
@@ -118,13 +118,13 @@ def generateTimeSeries(epochPD, tsFile, timeSeriesDateColumn = False,
             if 'MET' in e.columns:
                 labels.append('MET')
             e[['vmFinal','imputed'] + [l + 'Imputed' for l in labels]].to_csv(tsFile,
-                float_format='%.1f', index=timeSeriesDateColumn, 
+                float_format='%.1f', index=timeSeriesDateColumn,
                 header=[tsHead,'imputed']+labels, compression='gzip')
         else:
-            e[['vmFinal','imputed']].to_csv(tsFile, float_format='%.1f', 
+            e[['vmFinal','imputed']].to_csv(tsFile, float_format='%.1f',
                 index=timeSeriesDateColumn, header=[tsHead,'imputed'],
                 compression='gzip')
-        
+
     else:
         f = open(tsFile,'w')
         f.write(tsHead + '\n')
@@ -136,17 +136,17 @@ def generateTimeSeries(epochPD, tsFile, timeSeriesDateColumn = False,
 def loadTimeSeriesCSV(tsFile):
     """Load time series csv.gz file and append date/time column to it
 
-    The time associated with each reading can be inferred from the very first 
+    The time associated with each reading can be inferred from the very first
     row, which describes the sample rate, start and end times of the data.
 
     For example header
-    "acceleration (mg) - 2014-05-07 13:29:50 - 2014-05-13 09:50:25 - sampleRate = 5 seconds, imputed" 
-    indicates that the first measurement time is at 2014-05-07 13:29:50, the second 
-    at 2014-05-07 13:29:55, the third at 2014-05-07 13:30:00 ... and the last at 
+    "acceleration (mg) - 2014-05-07 13:29:50 - 2014-05-13 09:50:25 - sampleRate = 5 seconds, imputed"
+    indicates that the first measurement time is at 2014-05-07 13:29:50, the second
+    at 2014-05-07 13:29:55, the third at 2014-05-07 13:30:00 ... and the last at
     2014-05-13 09:50:25.
 
     :param str tsFile: Output filename for .csv.gz file
-    
+
     :return: Pandas dataframe of epoch data
     :rtype: pandas.DataFrame
 
@@ -170,17 +170,17 @@ def loadTimeSeriesCSV(tsFile):
         sampleRate = headerInfo.split("sampleRate = ")[1].split(" ")[0]
 
     # read data
-    tsData = pd.read_csv(tsFile, skiprows=1, header=None, names=header.columns, 
+    tsData = pd.read_csv(tsFile, skiprows=1, header=None, names=header.columns,
         compression='gzip')
     if header.columns[0] != TIME_SERIES_COL:
-        tsData.index = pd.date_range(start=startDate, end=endDate, 
+        tsData.index = pd.date_range(start=startDate, end=endDate,
             freq=str(sampleRate) + 's')
     return tsData
 
 
 
-def writeStudyAccProcessCmds(studyDir, cmdsFile, runName="default", 
-        accExt="cwa", cmdOptions="", filesID="files.csv"):
+def writeStudyAccProcessCmds(studyDir, cmdsFile, runName="default",
+        accExt="cwa", cmdOptions=None, filesID="files.csv"):
     """Read files to process and write out list of processing commands
 
     This method assumes that a study directory structure has been created by the
@@ -195,7 +195,7 @@ def writeStudyAccProcessCmds(studyDir, cmdsFile, runName="default",
         stationary/ #temp store for features of stationary data for calibration
         clusterLogs/ #to store terminal output for each processed file
 
-    If files.csv exists, process files listed here. If not, all files in 
+    If files.csv exists, process files listed here. If not, all files in
     rawData/ are read and listed in files.csv
 
     Then an acc processing command is written for each file and written to cmdsFile
@@ -217,7 +217,7 @@ def writeStudyAccProcessCmds(studyDir, cmdsFile, runName="default",
     >>> accUtils.writeStudyAccProcessingCmds("/myStudy/", "myStudy-cmds.txt")
     <cmd options written to "myStudy-cmds.txt">
     """
-    
+
     # firstly check if files.csv list exists (if not, create it)
     filesCSV = studyDir + filesID
     if not os.path.exists(filesCSV):
@@ -256,7 +256,7 @@ def writeStudyAccProcessCmds(studyDir, cmdsFile, runName="default",
         txtWriter.write(' --timeSeriesFolder ' + timeSeriesDir)
         txtWriter.write(' --nonWearFolder ' + nonWearDir)
         txtWriter.write(' --stationaryFolder ' + stationaryDir)
-        if cmdOptions != "":
+        if cmdOptions is not None:
             txtWriter.write(' ' + cmdOptions)
         txtWriter.write('\n')
     txtWriter.close()
@@ -282,7 +282,7 @@ def collateJSONfilesToSingleCSV(inputJsonDir, outputCsvFile):
     >>> accUtils.collateJSONfilesToSingleCSV("data/", "data/summary-all-files.csv")
     <summary CSV of all participants/files written to "data/sumamry-all-files.csv">
     """
-    
+
     ### First combine into <tmpJsonFile> the processed outputs from <inputJsonDir>
     tmpJsonFile = outputCsvFile.replace('.csv','-tmp.json')
     count = 0
@@ -314,11 +314,11 @@ def collateJSONfilesToSingleCSV(inputJsonDir, outputCsvFile):
 def identifyUnprocessedFiles(filesCsv, summaryCsv, outputFilesCsv):
     """identify files that have not been processed
 
-    Look through all processed accelerometer files, and find participants who do 
+    Look through all processed accelerometer files, and find participants who do
     not have records in the summary csv file. This indicates there was a problem
-    in processing their data. Therefore, output will be a new .csv file to 
+    in processing their data. Therefore, output will be a new .csv file to
     support reprocessing of these files
-    
+
     :param str filesCsv: CSV listing acc files in study directory
     :param str summaryCsv: Summary CSV of processed dataset
     :param str outputFilesCsv: Output csv listing files to be reprocessed
@@ -328,7 +328,7 @@ def identifyUnprocessedFiles(filesCsv, summaryCsv, outputFilesCsv):
 
     :Example:
     >>> import accUtils
-    >>> accUtils.identifyUnprocessedFiles("study/files.csv", study/summary-all-files.csv", 
+    >>> accUtils.identifyUnprocessedFiles("study/files.csv", study/summary-all-files.csv",
         "study/files-reprocess.csv")
     <Output csv listing files to be reprocessed written to "study/files-reprocess.csv">
     """
@@ -340,7 +340,7 @@ def identifyUnprocessedFiles(filesCsv, summaryCsv, outputFilesCsv):
     output = output.rename(columns={'Unnamed: 1': ''})
     output.to_csv(outputFilesCsv, index=False)
 
-    print('Reprocessing for ', len(output), 'participants written to:', 
+    print('Reprocessing for ', len(output), 'participants written to:',
         outputFilesCsv)
 
 
@@ -348,13 +348,13 @@ def identifyUnprocessedFiles(filesCsv, summaryCsv, outputFilesCsv):
 def updateCalibrationCoefs(inputCsvFile, outputCsvFile):
     """read summary .csv file and update coefs for those with poor calibration
 
-    Look through all processed accelerometer files, and find participants that 
-    did not have good calibration data. Then assigns the calibration coefs from 
-    previous good use of a given device. Output will be a new .csv file to 
+    Look through all processed accelerometer files, and find participants that
+    did not have good calibration data. Then assigns the calibration coefs from
+    previous good use of a given device. Output will be a new .csv file to
     support reprocessing of uncalibrated files with new pre-specified calibration coefs.
-    
+
     :param str inputCsvFile: Summary CSV of processed dataset
-    :param str outputCsvFile: Output CSV of files to be reprocessed with new 
+    :param str outputCsvFile: Output CSV of files to be reprocessed with new
         calibration info
 
     :return: New file written to <outputCsvFile>
@@ -380,7 +380,7 @@ def updateCalibrationCoefs(inputCsvFile, outputCsvFile):
                'calibration-xSlope(g)','calibration-ySlope(g)','calibration-zSlope(g)',
                'calibration-xTemp(C)','calibration-yTemp(C)','calibration-zTemp(C)',
                'calibration-meanDeviceTemp(C)']
-    
+
     #print output CSV file with suggested calibration parameters
     noOtherUses = 0
     nextUses = 0
@@ -395,18 +395,18 @@ def updateCalibrationCoefs(inputCsvFile, outputCsvFile):
         # (when it had a 'good' calibration)
         prevUse = goodCal[calCols][(goodCal['file-deviceID']==device) & (goodCal['file-startTime']<startTime)].tail(1)
         try:
-            ofX, ofY, ofZ, slpX, slpY, slpZ, tmpX, tmpY, tmpZ, calTempAvg = prevUse.iloc[0] 
+            ofX, ofY, ofZ, slpX, slpY, slpZ, tmpX, tmpY, tmpZ, calTempAvg = prevUse.iloc[0]
             previousUses += 1
         except:
             nextUse = goodCal[calCols][(goodCal['file-deviceID']==device) & (goodCal['file-startTime']>startTime)].head(1)
             if len(nextUse)<1:
-                print('no other uses for this device at all: ', str(device), 
+                print('no other uses for this device at all: ', str(device),
                     str(participant))
                 noOtherUses += 1
                 continue
             nextUses += 1
-            ofX, ofY, ofZ, slpX, slpY, slpZ, tmpX, tmpY, tmpZ, calTempAvg = nextUse.iloc[0] 
-        
+            ofX, ofY, ofZ, slpX, slpY, slpZ, tmpX, tmpY, tmpZ, calTempAvg = nextUse.iloc[0]
+
         #now construct output
         out = participant + ','
         out += str(ofX) + ' ' + str(ofY) + ' ' + str(ofZ) + ','
@@ -419,7 +419,7 @@ def updateCalibrationCoefs(inputCsvFile, outputCsvFile):
     print('nextUses', nextUses)
     print('noOtherUses', noOtherUses)
 
-    print('Reprocessing for ', str(previousUses + nextUses), 
+    print('Reprocessing for ', str(previousUses + nextUses),
         'participants written to:', outputCsvFile)
 
 
@@ -427,9 +427,9 @@ def updateCalibrationCoefs(inputCsvFile, outputCsvFile):
 def writeFilesWithCalibrationCoefs(inputCsvFile, outputCsvFile):
     """read summary .csv file and write files.csv with calibration coefs
 
-    Look through all processed accelerometer files, and write a new .csv file to 
+    Look through all processed accelerometer files, and write a new .csv file to
     support reprocessing of files with pre-specified calibration coefs.
-    
+
     :param str inputCsvFile: Summary CSV of processed dataset
     :param str outputCsvFile: Output CSV of files to process with calibration info
 
@@ -444,19 +444,19 @@ def writeFilesWithCalibrationCoefs(inputCsvFile, outputCsvFile):
     """
 
     d = pd.read_csv(inputCsvFile)
-    
+
     calCols = ['calibration-xOffset(g)','calibration-yOffset(g)','calibration-zOffset(g)',
                'calibration-xSlope(g)','calibration-ySlope(g)','calibration-zSlope(g)',
                'calibration-xTemp(C)','calibration-yTemp(C)','calibration-zTemp(C)',
                'calibration-meanDeviceTemp(C)']
-    
+
     #print output CSV file with suggested calibration parameters
     f = open(outputCsvFile,'w')
     f.write('fileName,calOffset,calSlope,calTemp,meanTemp\n')
     for ix, row in d.iterrows():
         #first get current file information
         participant = str(row['file-name'])
-        ofX, ofY, ofZ, slpX, slpY, slpZ, tmpX, tmpY, tmpZ, calTempAvg = row[calCols] 
+        ofX, ofY, ofZ, slpX, slpY, slpZ, tmpX, tmpY, tmpZ, calTempAvg = row[calCols]
         #now construct output
         out = participant + ','
         out += str(ofX) + ' ' + str(ofY) + ' ' + str(ofZ) + ','
@@ -466,7 +466,7 @@ def writeFilesWithCalibrationCoefs(inputCsvFile, outputCsvFile):
         f.write(out + '\n')
     f.close()
 
-    print('Files with calibration coefficients for ', str(len(d)), 
+    print('Files with calibration coefficients for ', str(len(d)),
         'participants written to:', outputCsvFile)
 
 
@@ -474,7 +474,7 @@ def writeFilesWithCalibrationCoefs(inputCsvFile, outputCsvFile):
 def createDirIfNotExists(folder):
     """ Create directory if it doesn't currently exist
 
-    :param str folder: Directory to be checked/created 
+    :param str folder: Directory to be checked/created
 
     :return: Dir now exists (created if didn't exist before, otherwise untouched)
     :rtype: void
