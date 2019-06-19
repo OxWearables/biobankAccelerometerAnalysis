0998696d89a7f1799aee44484826ba5a7ffaf337
Aiden Doherty <aiden.doherty@bdi.ox.ac.uk>
2019-06-19 22:04:18 +0100

    refactor CWA reading code, to make more flexible and easier to maintain. This does result in a ~6% slowdown, but makes the code slightly easier to follow

--- java/AxivityAx3Epochs.java
+++ java/AxivityAx3Epochs.java
@@ -25,8 +25,6 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
-
-
 import java.util.TimeZone;
 import java.time.Duration;
 import java.time.format.DateTimeFormatter;
@@ -655,7 +653,6 @@ public class AxivityAx3Epochs {
 	}
 
 
-
 	/*
 	 * This method checks if a file is of GT3X format version 1 It returns true
 	 * if the file is of the correct format otherwise it returns false
@@ -682,14 +679,11 @@ public class AxivityAx3Epochs {
 		return false;
 	}
 
+
+
 	/**
-	 * Read Axivity CWA file, then call method to write epochs from raw data.
-	 * Epochs will be written to epochFileWriter.
-	 * Read data block HEX values, store each raw reading, then continually test
-	 * if an epoch of data has been collected or not. Finally, write each epoch
-	 * to epochFileWriter. Method also updates and returns epochStartTime. CWA
-	 * format is described at:
-     * https://github.com/digitalinteraction/openmovement/blob/master/Downloads/AX3/AX3-CWA-Format.txt
+	 * Read and process Axivity CWA file. Setup file reading infrastructure
+	 * and then call readCwaBuffer() method
 	**/
 	private static void readCwaEpochs(String accFile) {
 
@@ -720,212 +714,9 @@ public class AxivityAx3Epochs {
 												// interpolates timestamp
 												// between blocks.
 			while (rawAccReader.read(buf) != -1) {
-				buf.flip();
-				buf.order(ByteOrder.LITTLE_ENDIAN);
-				header = (char) buf.get() + "";
-				header += (char) buf.get() + "";
-				if (header.equals("MD")) {
-					// Read first page (& data-block) to get time, temp,
-					// measureFreq
-					// start-epoch values
-					try {
-						SESSION_START = cwaHeaderLoggingStartTime(buf);
-						System.out.println("Session start:" + SESSION_START);
-					} catch (Exception e) {
-						System.err.println("No preset start time");
-					}
-				} else if (header.equals("AX")) {
-					// read each individual page block, and process epochs...
-					try {
-
-						// read block header items
-						long blockTimestamp = getUnsignedInt(buf, 14);
-						int light = getUnsignedShort(buf, 18);
-						double temperature = (getUnsignedShort(buf, 20) * 150.0 - 20500) / 1000;
-						short rateCode = (short) (buf.get(24) & 0xff);
-						short numAxesBPS = (short) (buf.get(25) & 0xff);
-						int sampleCount = getUnsignedShort(buf, 28);
-						short timestampOffset = 0;
-						double sampleFreq = 0;
-						int fractional = 0; // 1/65536th of a second fractions
-
-						// check not very old file as pos 26=freq rather than
-						// timestamp offset
-						if (rateCode != 0) {
-							timestampOffset = buf.getShort(26); // timestamp
-																// offset ok
-							// if fractional offset, then timestamp offset was
-							// artificially
-							// modified for backwards-compatibility ...
-							// therefore undo this...
-							int oldDeviceId = getUnsignedShort(buf, 4);
-							if ((oldDeviceId & 0x8000) != 0) {
-								sampleFreq = 3200.0 / (1 << (15 - (rateCode & 15)));
-								if (USE_PRECISE_TIME) {
-									// Need to undo backwards-compatible shim:
-									// Take into account how many whole samples
-									// the fractional part of timestamp
-									// accounts for:
-									// relativeOffset = fifoLength -
-									// (short)(((unsigned long)timeFractional *
-									// AccelFrequency()) >> 16);
-									// nearest whole sample
-									// whole-sec | /fifo-pos@time
-									// | |/
-									// [0][1][2][3][4][5][6][7][8][9]
-									// use 15-bits as 16-bit fractional time
-									fractional = ((oldDeviceId & 0x7fff) << 1);
-									// frequency is truncated to int in firmware
-									timestampOffset += ((fractional * (int) sampleFreq) >> 16);
-								}
-							}
-						} else {
-							sampleFreq = buf.getShort(26);
-							// very old format, where pos26 = freq
-						}
-
-						// calculate num bytes per sample...
-						byte bytesPerSample = 4;
-						int NUM_AXES_PER_SAMPLE = 3;
-						if ((numAxesBPS & 0x0f) == 2) {
-							bytesPerSample = 6; // 3*16-bit
-						} else if ((numAxesBPS & 0x0f) == 0) {
-							bytesPerSample = 4; // 3*10-bit + 2
-						}
-
-						// Limit values
-						int maxSamples = 480 / bytesPerSample; // 80 or 120
-																// samples/block
-						if (sampleCount > maxSamples) {
-							sampleCount = maxSamples;
-						}
-						if (sampleFreq <= 0) {
-							sampleFreq = 1;
-						}
-
-						// determine time for indexed sample within block
-						LocalDateTime blockTime = getCwaTimestamp((int) blockTimestamp, fractional);
-						// first & last sample. Actually, last = first sample in
-						// next block
-						LocalDateTime firstSampleTime, lastSampleTime;
-						// if no interval between times (or interval too large)
-						long spanToSample = 0;
-						if (lastBlockTime[0] != null) {
-							spanToSample = Duration.between(lastBlockTime[0], blockTime).toNanos();
-						}
-						if (!USE_PRECISE_TIME ||
-								lastBlockTime[0] == null ||
-								timestampOffset <= lastBlockTimeIndex[0] ||
-								spanToSample <= 0 ||
-								spanToSample > 1000000000.0 * 2 * maxSamples / sampleFreq
-							) {
-							float offsetStart = (float) -timestampOffset / (float) sampleFreq;
-							firstSampleTime = blockTime.plusNanos(secs2Nanos(offsetStart));
-							lastSampleTime = firstSampleTime.plusNanos(secs2Nanos(sampleCount / sampleFreq));
-						} else {
-							double gap = (double) spanToSample / (-lastBlockTimeIndex[0] + timestampOffset);
-							firstSampleTime = lastBlockTime[0].plusNanos((long) (-lastBlockTimeIndex[0] * gap));
-							lastSampleTime = lastBlockTime[0]
-									.plusNanos((long) ((-lastBlockTimeIndex[0] + sampleCount) * gap));
-						}
-
-
-						// Last block time
-						lastBlockTime[0] = blockTime;
-						// Advance last block time index for next block
-						lastBlockTimeIndex[0] = timestampOffset - sampleCount;
-						// Overall span of block
-						long spanNanos = Duration.between(firstSampleTime, lastSampleTime).toNanos();
-
-						// set target epoch start time of very first block
-						/*TODO:is this needed anymore?
-						if (epochStartTime == null) {
-							epochStartTime = firstSampleTime;
-							// if set, clamp session to intended log start time
-							if (SESSION_START != null) {
-								START_OFFSET_NANOS = Duration.between(epochStartTime, SESSION_START).toNanos();
-								// check block time and session start time are
-								// within 10secs
-								long clampLimitNanos = secs2Nanos(15);
-								if (START_OFFSET_NANOS > clampLimitNanos || START_OFFSET_NANOS < -clampLimitNanos) {
-									START_OFFSET_NANOS = 0;
-									System.out.println("Can't clamp to logging start time");
-								}
-							}
-						}
-						*/
-
-						// raw reading values
-						long value = 0; // x/y/z vals
-						short xRaw = 0;
-						short yRaw = 0;
-						short zRaw = 0;
-						double x = 0.0;
-						double y = 0.0;
-						double z = 0.0;
-
-						// loop through each line in data block and check if it
-						// is last in
-						// epoch then write epoch summary to file
-						// an epoch will have a start+end time, and fixed
-						// duration
-						for (int i = 0; i < sampleCount; i++) {
-							if (USE_PRECISE_TIME) {
-								// Calculate each sample's time, not successively adding
-								// so that we don't accumulate any errors
-								blockTime = firstSampleTime.plusNanos((long) (i * (double) spanNanos / sampleCount));
-							} else {
-
-								if (i == 0) {
-									blockTime = firstSampleTime; // emulate original
-																	// behaviour
-								} else {
-									blockTime = blockTime.plusNanos(secs2Nanos(1.0 / sampleFreq));
-								}
-							}
-
-							if (bytesPerSample == 4) {
-								try {
-									value = getUnsignedInt(buf, 30 + 4 * i);
-								} catch (Exception excep) {
-									errCounter[0] += 1;
-									System.err.println("xyz reading err: " + excep.toString());
-									break; // rest of block/page may be
-											// corrupted
-								}
-								// Sign-extend 10-bit values, adjust for
-								// exponents
-								xRaw = (short) ((short) (0xffffffc0 & (value << 6)) >> (6 - ((value >> 30) & 0x03)));
-								yRaw = (short) ((short) (0xffffffc0 & (value >> 4)) >> (6 - ((value >> 30) & 0x03)));
-								zRaw = (short) ((short) (0xffffffc0 & (value >> 14)) >> (6 - ((value >> 30) & 0x03)));
-							} else if (bytesPerSample == 6) {
-								try {
-									xRaw = buf.getShort(30 + 2 * NUM_AXES_PER_SAMPLE * i + 0);
-									yRaw = buf.getShort(30 + 2 * NUM_AXES_PER_SAMPLE * i + 2);
-									zRaw = buf.getShort(30 + 2 * NUM_AXES_PER_SAMPLE * i + 4);
-								} catch (Exception excep) {
-									errCounter[0] += 1;
-									System.err.println("xyz read err: " + excep.toString());
-									break; // rest of block/page may be
-											// corrupted
-								}
-							} else {
-								xRaw = 0;
-								yRaw = 0;
-								zRaw = 0;
-							}
-							x = xRaw / 256.0;
-							y = yRaw / 256.0;
-							z = zRaw / 256.0;
-
-							epochWriter.newValues(getEpochMillis(blockTime.plusNanos(START_OFFSET_NANOS)), x, y, z, temperature, errCounter);
-
-						}
-					} catch (Exception excep) {
-						excep.printStackTrace(System.err);
-						System.err.println("block err @ " + lastBlockTime[0].toString() + ": " + excep.toString());
-					}
-				}
+				readCwaBuffer(buf, SESSION_START, START_OFFSET_NANOS, 
+					USE_PRECISE_TIME, lastBlockTime, lastBlockTimeIndex, header,
+					errCounter);
 				buf.clear();
 				// option to provide status update to user...
 				pageCount++;
@@ -940,6 +731,212 @@ public class AxivityAx3Epochs {
 			System.exit(-2);
 		}
 	}
+
+
+	/**
+	 * Read Axivity CWA file, then call method to write epochs from raw data.
+	 * Epochs will be written to epochFileWriter.
+	 * Read data block HEX values, store each raw reading, then continually test
+	 * if an epoch of data has been collected or not. Finally, write each epoch
+	 * to epochFileWriter. CWA format is described at:
+     * https://github.com/digitalinteraction/openmovement/blob/master/Downloads/AX3/AX3-CWA-Format.txt
+	**/
+	private static void readCwaBuffer(ByteBuffer buf, LocalDateTime SESSION_START,
+		long START_OFFSET_NANOS, boolean USE_PRECISE_TIME, 
+		LocalDateTime[] lastBlockTime, int[] lastBlockTimeIndex, String header,
+		int[] errCounter)
+	{
+		buf.flip();
+		buf.order(ByteOrder.LITTLE_ENDIAN);
+		header = (char) buf.get() + "";
+		header += (char) buf.get() + "";
+		if (header.equals("MD")) {
+			// Read first page (& data-block) to get time, temp,
+			// measureFreq
+			// start-epoch values
+			try {
+				SESSION_START = cwaHeaderLoggingStartTime(buf);
+				System.out.println("Session start:" + SESSION_START);
+			} catch (Exception e) {
+				System.err.println("No preset start time");
+			}
+		} else if (header.equals("AX")) {
+			// read each individual page block, and process epochs...
+			try {
+
+				// read block header items
+				long blockTimestamp = getUnsignedInt(buf, 14);
+				int light = getUnsignedShort(buf, 18);
+				double temperature = (getUnsignedShort(buf, 20) * 150.0 - 20500) / 1000;
+				short rateCode = (short) (buf.get(24) & 0xff);
+				short numAxesBPS = (short) (buf.get(25) & 0xff);
+				int sampleCount = getUnsignedShort(buf, 28);
+				short timestampOffset = 0;
+				double sampleFreq = 0;
+				int fractional = 0; // 1/65536th of a second fractions
+
+				// check not very old file as pos 26=freq rather than
+				// timestamp offset
+				if (rateCode != 0) {
+					timestampOffset = buf.getShort(26); // timestamp
+														// offset ok
+					// if fractional offset, then timestamp offset was
+					// artificially
+					// modified for backwards-compatibility ...
+					// therefore undo this...
+					int oldDeviceId = getUnsignedShort(buf, 4);
+					if ((oldDeviceId & 0x8000) != 0) {
+						sampleFreq = 3200.0 / (1 << (15 - (rateCode & 15)));
+						if (USE_PRECISE_TIME) {
+							// Need to undo backwards-compatible shim:
+							// Take into account how many whole samples
+							// the fractional part of timestamp
+							// accounts for:
+							// relativeOffset = fifoLength -
+							// (short)(((unsigned long)timeFractional *
+							// AccelFrequency()) >> 16);
+							// nearest whole sample
+							// whole-sec | /fifo-pos@time
+							// | |/
+							// [0][1][2][3][4][5][6][7][8][9]
+							// use 15-bits as 16-bit fractional time
+							fractional = ((oldDeviceId & 0x7fff) << 1);
+							// frequency is truncated to int in firmware
+							timestampOffset += ((fractional * (int) sampleFreq) >> 16);
+						}
+					}
+				} else {
+					sampleFreq = buf.getShort(26);
+					// very old format, where pos26 = freq
+				}
+
+				// calculate num bytes per sample...
+				byte bytesPerSample = 4;
+				int NUM_AXES_PER_SAMPLE = 3;
+				if ((numAxesBPS & 0x0f) == 2) {
+					bytesPerSample = 6; // 3*16-bit
+				} else if ((numAxesBPS & 0x0f) == 0) {
+					bytesPerSample = 4; // 3*10-bit + 2
+				}
+
+				// Limit values
+				int maxSamples = 480 / bytesPerSample; // 80 or 120
+														// samples/block
+				if (sampleCount > maxSamples) {
+					sampleCount = maxSamples;
+				}
+				if (sampleFreq <= 0) {
+					sampleFreq = 1;
+				}
+
+				// determine time for indexed sample within block
+				LocalDateTime blockTime = getCwaTimestamp((int) blockTimestamp, fractional);
+				// first & last sample. Actually, last = first sample in
+				// next block
+				LocalDateTime firstSampleTime, lastSampleTime;
+				// if no interval between times (or interval too large)
+				long spanToSample = 0;
+				if (lastBlockTime[0] != null) {
+					spanToSample = Duration.between(lastBlockTime[0], blockTime).toNanos();
+				}
+				if (!USE_PRECISE_TIME ||
+						lastBlockTime[0] == null ||
+						timestampOffset <= lastBlockTimeIndex[0] ||
+						spanToSample <= 0 ||
+						spanToSample > 1000000000.0 * 2 * maxSamples / sampleFreq
+					) {
+					float offsetStart = (float) -timestampOffset / (float) sampleFreq;
+					firstSampleTime = blockTime.plusNanos(secs2Nanos(offsetStart));
+					lastSampleTime = firstSampleTime.plusNanos(secs2Nanos(sampleCount / sampleFreq));
+				} else {
+					double gap = (double) spanToSample / (-lastBlockTimeIndex[0] + timestampOffset);
+					firstSampleTime = lastBlockTime[0].plusNanos((long) (-lastBlockTimeIndex[0] * gap));
+					lastSampleTime = lastBlockTime[0]
+							.plusNanos((long) ((-lastBlockTimeIndex[0] + sampleCount) * gap));
+				}
+
+
+				// Last block time
+				lastBlockTime[0] = blockTime;
+				// Advance last block time index for next block
+				lastBlockTimeIndex[0] = timestampOffset - sampleCount;
+				// Overall span of block
+				long spanNanos = Duration.between(firstSampleTime, lastSampleTime).toNanos();
+
+				// raw reading values
+				long value = 0; // x/y/z vals
+				short xRaw = 0;
+				short yRaw = 0;
+				short zRaw = 0;
+				double x = 0.0;
+				double y = 0.0;
+				double z = 0.0;
+
+				// loop through each line in data block and check if it
+				// is last in
+				// epoch then write epoch summary to file
+				// an epoch will have a start+end time, and fixed
+				// duration
+				for (int i = 0; i < sampleCount; i++) {
+					if (USE_PRECISE_TIME) {
+						// Calculate each sample's time, not successively adding
+						// so that we don't accumulate any errors
+						blockTime = firstSampleTime.plusNanos((long) (i * (double) spanNanos / sampleCount));
+					} else {
+
+						if (i == 0) {
+							blockTime = firstSampleTime; // emulate original
+															// behaviour
+						} else {
+							blockTime = blockTime.plusNanos(secs2Nanos(1.0 / sampleFreq));
+						}
+					}
+
+					if (bytesPerSample == 4) {
+						try {
+							value = getUnsignedInt(buf, 30 + 4 * i);
+						} catch (Exception excep) {
+							errCounter[0] += 1;
+							System.err.println("xyz reading err: " + excep.toString());
+							break; // rest of block/page may be
+									// corrupted
+						}
+						// Sign-extend 10-bit values, adjust for
+						// exponents
+						xRaw = (short) ((short) (0xffffffc0 & (value << 6)) >> (6 - ((value >> 30) & 0x03)));
+						yRaw = (short) ((short) (0xffffffc0 & (value >> 4)) >> (6 - ((value >> 30) & 0x03)));
+						zRaw = (short) ((short) (0xffffffc0 & (value >> 14)) >> (6 - ((value >> 30) & 0x03)));
+					} else if (bytesPerSample == 6) {
+						try {
+							xRaw = buf.getShort(30 + 2 * NUM_AXES_PER_SAMPLE * i + 0);
+							yRaw = buf.getShort(30 + 2 * NUM_AXES_PER_SAMPLE * i + 2);
+							zRaw = buf.getShort(30 + 2 * NUM_AXES_PER_SAMPLE * i + 4);
+						} catch (Exception excep) {
+							errCounter[0] += 1;
+							System.err.println("xyz read err: " + excep.toString());
+							break; // rest of block/page may be
+									// corrupted
+						}
+					} else {
+						xRaw = 0;
+						yRaw = 0;
+						zRaw = 0;
+					}
+					x = xRaw / 256.0;
+					y = yRaw / 256.0;
+					z = zRaw / 256.0;
+
+					epochWriter.newValues(getEpochMillis(blockTime.plusNanos(START_OFFSET_NANOS)), x, y, z, temperature, errCounter);
+
+				}
+			} catch (Exception excep) {
+				excep.printStackTrace(System.err);
+				System.err.println("block err @ " + lastBlockTime[0].toString() + ": " + excep.toString());
+			}
+		}
+	}
+
+
 
 	// Parse HEX values, CWA format is described at:
     // https://github.com/digitalinteraction/openmovement/blob/master/Downloads/AX3/AX3-CWA-Format.txt
