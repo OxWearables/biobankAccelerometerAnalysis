9f6e6360f03fdea705b4418eb1dc286d461dc1e3
egctong <egctong@gmail.com>
2020-02-04 15:22:25 +0000

    Fix bug in getCalibrationCoefs
    storeCalibrationInformation not called when returned early due to insufficient measurements

--- accelerometer/device.py
+++ accelerometer/device.py
@@ -219,8 +219,9 @@ def getCalibrationCoefs(staticBoutsFile, summary):
         d = np.loadtxt(open(staticBoutsFile,"rb"),delimiter=",",skiprows=1,
                 usecols=(2,3,4,11,13))
         if len(d)<=5:
-            return [0.0,0.0,0.0], [1.0,1.0,1.0], [0.0,0.0,0.0], 20, np.nan, np.nan, \
-                np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, len(d)
+            storeCalibrationInformation(summary, [0.0,0.0,0.0], [1.0,1.0,1.0], [0.0,0.0,0.0], 20, np.nan, np.nan,
+                np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, len(d))
+            return
         stationaryPoints = d[d[:,4] == 0] # don't consider episodes with data errors
         axesVals = stationaryPoints[:,[0,1,2]]
         tempVals = stationaryPoints[:,[3]]
