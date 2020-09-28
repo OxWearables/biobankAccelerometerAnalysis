0cc8fce8976aa8bd3e8c28fa1e9cb21ae200c458
angerhang <514140809@qq.com>
2020-09-28 23:10:01 +0100

    try fixing ci again
    change ci names
    
    update gt3x tests
    
    test gt3x tests
    
    more gt3x test
    
    this should fail
    
    this shall fail
    
    this shall work
    
    better test documentations
    
    typos

--- java/Tests/Resources/compareOutput.sh
+++ java/Tests/Resources/compareOutput.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ $(diff java/Tests/Resources/sampleV2Epoch.csv java/Tests/Resources/sampleV2.gt3x) ]]; then
-  exit 0
-else
+if [[ $(diff java/Tests/Resources/sampleV2Epoch.csv java/Tests/Resources/sampleV2EpochRef.csv) ]]; then
   exit 1
+else
+  exit 0
 fi
