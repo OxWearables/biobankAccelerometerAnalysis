45c540dd2852e33b5eeb3c2e0c5401bd093ec145
angerhang <514140809@qq.com>
2020-09-30 21:34:23 +0100

    add junit framework

---
+++ java/Tests/SampleTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SampleTest {
+    @Test
+    public void testAdd() {
+        String str = "Junit is working fine";
+        assertEquals("Junit is working fine", str);
+    }
+}
