1da70728afde889ed1a5b7af124836fca615177f
Shing Chan <cshing.m@gmail.com>
2021-10-12 10:22:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="accelerometer",
-    version="3.1",
+    version="3.2",
     author="Aiden Doherty",
     author_email="aiden.doherty@bdi.ox.ac.uk",
     description="A package to extract meaningful health information from large accelerometer datasets e.g. how much time individuals spend in sleep, sedentary behaviour, walking and moderate intensity physical activity",
