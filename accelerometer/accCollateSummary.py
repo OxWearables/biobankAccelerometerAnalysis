b91b00146a8d3993c1a5b3f01916651cf059ac75
Shing Chan <cshing.m@gmail.com>
2021-11-09 14:03:19 +0000

    create accWriteCmds.py, accCollateSummary.py

---
+++ accelerometer/accCollateSummary.py
@@ -0,0 +1,16 @@
+import argparse
+from accelerometer.accUtils import collateSummary
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('resultsDir')
+    parser.add_argument('--outputCsvFile', '-o', default="all-summary.csv")
+    args = parser.parse_args()
+
+    collateSummary(resultsDir=args.resultsDir,
+                   outputCsvFile=args.outputCsvFile)
+
+
+if __name__ == '__main__':
+    main()
