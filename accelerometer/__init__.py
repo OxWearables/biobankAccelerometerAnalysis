6029c4bc4f0f9a8e43b4264d46dc911752beed2c
Shing Chan <cshing.m@gmail.com>
2022-06-21 20:34:42 +0100

    v5.1.2

--- accelerometer/__init__.py
+++ accelerometer/__init__.py
@@ -1,5 +1,5 @@
 name = "accelerometer"
-__version__ = "5.1.1"
+__version__ = "5.1.2"
 __author__ = "Aiden Doherty, Shing Chan, Rosemary Walmsley, Hang Yuan, Dan Jackson, Nils Hammerla"
 __email__ = "aiden.doherty@ndph.ox.ac.uk, shing.chan@ndph.ox.ac.uk, rosemary.walmsley@gtc.ox.ac.uk, hang.yuan@keble.ox.ac.uk, dan.jackson@ncl.ac.uk, nils.hammerla@newcastle.ac.uk"
 __license__ = "2-Clause BSD"
