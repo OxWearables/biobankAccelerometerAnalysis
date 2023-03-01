4c9f668f62d86ae473544f12458712e737c3aa84
Shing Chan <cshing.m@gmail.com>
2023-03-01 13:41:58 +0000

    restruct model directories & url; split chan & chanw models

--- accelerometer/models.py
+++ accelerometer/models.py
@@ -1,48 +1,35 @@
 import pathlib
 
-ROOT_DIR = pathlib.Path(__file__).parent
-MODEL_VER = "10Feb2022"
-MODEL_DIR = ROOT_DIR / "models"
-MODEL_URL = "https://wearables-files.ndph.ox.ac.uk/files/models"
+MODEL_DIR = pathlib.Path(__file__).parent / "models"
+MODEL_ROOT_URL = "https://wearables-files.ndph.ox.ac.uk/files/models/biobankAccelerometerAnalysis"
 
 
 MODELS = {
+
     'willetts': {
-        "pth": MODEL_DIR / MODEL_VER / "willetts/model.tar",
-        "url": f"{MODEL_URL}/{MODEL_VER}/willetts/model.tar",
-    },
-    'doherty': {
-        "pth": MODEL_DIR / MODEL_VER / "doherty/model.tar",
-        "url": f"{MODEL_URL}/{MODEL_VER}/doherty/model.tar",
-    },
-    'walmsley': {
-        "pth": MODEL_DIR / MODEL_VER / "walmsley/model.tar",
-        "url": f"{MODEL_URL}/{MODEL_VER}/walmsley/model.tar",
-    },
-    'chan': {
-        "pth": MODEL_DIR / "06Jan2023" / "chan/model.tar",
-        "url": "https://tinyurl.com/5csujv8r",
+        "pth": MODEL_DIR / "willetts" / "20220210.tar",
+        "url": f"{MODEL_ROOT_URL}/willetts/20220210.tar",
     },
 
-    'willetts-10Feb2022': {
-        "pth": MODEL_DIR / "10Feb2022" / "willetts/model.tar",
-        "url": f"{MODEL_URL}/10Feb2022/willetts/model.tar",
+    'doherty': {
+        "pth": MODEL_DIR / "doherty" / "20220210.tar",
+        "url": f"{MODEL_ROOT_URL}/doherty/20220210.tar",
     },
-    'doherty-10Feb2022': {
-        "pth": MODEL_DIR / "10Feb2022" / "doherty/model.tar",
-        "url": f"{MODEL_URL}/10Feb2022/doherty/model.tar",
+
+    'walmsley': {
+        "pth": MODEL_DIR / "walmsley" / "20220210.tar",
+        "url": f"{MODEL_ROOT_URL}/walmsley/20220210.tar",
     },
-    'walmsley-10Feb2022': {
-        "pth": MODEL_DIR / "10Feb2022" / "walmsley/model.tar",
-        "url": f"{MODEL_URL}/10Feb2022/walmsley/model.tar",
+
+    'chan': {
+        "pth": MODEL_DIR / "chan" / "20230103.tar",
+        "url": f"{MODEL_ROOT_URL}/chan/20230103.tar",
+
     },
-    'chan-01Jan2023': {
-        "pth": MODEL_DIR / "01Jan2023" / "chan/model.tar",
-        "url": "https://tinyurl.com/yuzn99zr",
+
+    'chanw': {
+        "pth": MODEL_DIR / "chanw" / "20230106.tar",
+        "url": f"{MODEL_ROOT_URL}/chanw/20230106.tar"
     },
-    'chan-06Jan2023': {
-        "pth": MODEL_DIR / "06Jan2023" / "chan/model.tar",
-        "url": "https://tinyurl.com/5csujv8r",
-    }
 
 }
