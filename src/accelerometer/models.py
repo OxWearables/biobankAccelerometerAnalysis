6b91c940dacd0715c1d42114898ad86ec87cf8b1
Shing Chan <cshing.m@gmail.com>
2023-07-24 14:56:12 +0100

    update models after sklearn
    Re-pickled models after recent sklearn update to 1.0.2 to omit warning.

--- src/accelerometer/models.py
+++ src/accelerometer/models.py
@@ -7,29 +7,28 @@ MODEL_ROOT_URL = "https://wearables-files.ndph.ox.ac.uk/files/models/biobankAcce
 MODELS = {
 
     'willetts': {
-        "pth": MODEL_DIR / "willetts" / "20220210.tar",
-        "url": f"{MODEL_ROOT_URL}/willetts/20220210.tar",
+        "pth": MODEL_DIR / "willetts" / "20220210_sk_1_0_2.tar",
+        "url": f"{MODEL_ROOT_URL}/willetts/20220210_sk_1_0_2.tar",
     },
 
     'doherty': {
-        "pth": MODEL_DIR / "doherty" / "20220210.tar",
-        "url": f"{MODEL_ROOT_URL}/doherty/20220210.tar",
+        "pth": MODEL_DIR / "doherty" / "20220210_sk_1_0_2.tar",
+        "url": f"{MODEL_ROOT_URL}/doherty/20220210_sk_1_0_2.tar",
     },
 
     'walmsley': {
-        "pth": MODEL_DIR / "walmsley" / "20220210.tar",
-        "url": f"{MODEL_ROOT_URL}/walmsley/20220210.tar",
+        "pth": MODEL_DIR / "walmsley" / "20220210_sk_1_0_2.tar",
+        "url": f"{MODEL_ROOT_URL}/walmsley/20220210_sk_1_0_2.tar",
     },
 
     'chan': {
-        "pth": MODEL_DIR / "chan" / "20230103.tar",
-        "url": f"{MODEL_ROOT_URL}/chan/20230103.tar",
-
+        "pth": MODEL_DIR / "chan" / "20230103_sk_1_0_2.tar",
+        "url": f"{MODEL_ROOT_URL}/chan/20230103_sk_1_0_2.tar",
     },
 
     'chanw': {
-        "pth": MODEL_DIR / "chanw" / "20230106.tar",
-        "url": f"{MODEL_ROOT_URL}/chanw/20230106.tar"
+        "pth": MODEL_DIR / "chanw" / "20230106_sk_1_0_2.tar",
+        "url": f"{MODEL_ROOT_URL}/chanw/20230106_sk_1_0_2.tar"
     },
 
 }
